8a9cb2822a9d034a367d948bd39b8008ab16361b
David Diggs <ddiggs34@gmail.com>
2023-10-23 20:28:07 -0400

    done

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+
+        public bool IsAPalindrome(string word)
+        {
+            var palindrome = "";
+            
+            for (int i = word.Length - 1; i >= 0; i--) //formula to go reversed
+            {
+                palindrome += word[i];
+            }
+
+            if (word.ToUpper() == palindrome.ToUpper())
+            {
+                return true;
+            }
+            else
+            { return false; }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("rotator", true)]
+        [InlineData("wow", true)]
+        [InlineData("noon", true)]
+        [InlineData("bingo", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //ARRANGE
+            var wordsmith = new Wordsmith();
+            //ACT
+            var actual = wordsmith.IsAPalindrome(word);
+            //ASSERT
+            Assert.Equal(expected, actual);
 
         }
     }
